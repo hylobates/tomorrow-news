3827b7e84a8220750c45c7587eb227276f469ecf
KAIDEN <KAIDEN>
2016-11-02 01:35:55 +0800

    byKun

---
+++ libs/src/js/index.js
@@ -0,0 +1,8 @@
+ window.onload = function() {
+ 	var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 5,
+        paginationClickable: true,
+        spaceBetween: 0
+    });	// body...
+ }
