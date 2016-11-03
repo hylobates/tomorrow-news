533183776213b789e29fe5465452f8eeac40c01a
吴培锟 <951301664@qq.com>
2016-11-03 21:48:24 +0800

    byKun

--- libs/src/js/index.js
+++ libs/src/js/index.js
@@ -6,3 +6,19 @@
         spaceBetween: 0
     });	// body...
  }
+
+var app = angular.module('connect', []);
+app.controller('newsconnect', ['$scope','$http' ,function($scope,$http){
+	$scope.obj = [];
+	$http({
+		url:'http://apis.baidu.com/showapi_open_bus/channel_news/search_news',
+		param:{'_':Math.random()},
+		headers:{'apikey':'51f12c20e568ed12f876326143a2f412'}
+	}).success(function(response){
+		console.log(response);
+		if(response){
+			$scope.obj = response.showapi_res_body.pagebean.contentlist;
+			console.log($scope.obj);
+		}
+	})
+}])
