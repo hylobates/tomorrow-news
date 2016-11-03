f97253c3cc9d33d67055f1505ea7df4670172fb7
KAIDEN <KAIDEN>
2016-11-04 01:54:09 +0800

    byKUN

--- libs/src/js/index.js
+++ libs/src/js/index.js
@@ -10,6 +10,7 @@
 var app = angular.module('connect', []);
 app.controller('newsconnect', ['$scope','$http' ,function($scope,$http){
 	$scope.obj = [];
+	$scope.imgUrl = [];
 	$http({
 		url:'http://apis.baidu.com/showapi_open_bus/channel_news/search_news',
 		param:{'_':Math.random()},
@@ -19,6 +20,17 @@ app.controller('newsconnect', ['$scope','$http' ,function($scope,$http){
 		if(response){
 			$scope.obj = response.showapi_res_body.pagebean.contentlist;
 			console.log($scope.obj);
+			$scope.obj.forEach(function(val){
+				// console.log(val);
+				if(val.imageurls[0]){
+					// console.log(val.imageurls[0].url);
+					$scope.imgUrl.push({'key':val.imageurls[0].url});
+				}else{
+					$scope.imgUrl.push({'key':''});
+				}
+				// $scope.imgUrl = JSON.parse(JSON.stringify($scope.imgUrl));
+				console.log($scope.imgUrl);
+			})
 		}
 	})
 }])
